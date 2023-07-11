b2f750d0f04b4a056c10f87f5198af8123dc4026
Mickie Zhou <mickiedd@gmail.com>
2023-07-11 23:17:46 +0800

    Fix indentations.

--- SkillProj/Private/SkillCtrlComp.cpp
+++ SkillProj/Private/SkillCtrlComp.cpp
@@ -21,8 +21,8 @@ USkillCtrlComp::USkillCtrlComp()
 // load json file, and initialize.
 void USkillCtrlComp::LoadSkillData(FSkillJsonData& InSkillJsonData)
 {
-    // Clear SkillPhases
-    ClearSkillPhases();
+	// Clear SkillPhases
+	ClearSkillPhases();
 
 	// assume InSkillJsonData.SkillPath = "GameJson.Skills.Dash", translate it to "/Game/GameJson/Skills/Dash.json"
 	FString SkillJsonPath = FPaths::ProjectContentDir() + InSkillJsonData.SkillPath.Replace(TEXT("."), TEXT("/")) + FString(".json");
@@ -36,62 +36,62 @@ void USkillCtrlComp::LoadSkillData(FSkillJsonData& InSkillJsonData)
 	{
 		// Successfully deserialized JSON data
 		UE_LOG(LogTemp, Log, TEXT("Successfully deserialized JSON data"));
-        // Assume the JsonString is already deserialized to a FJsonObject called JsonObject
-        // First, extract the SkillName field from the JsonObject
-        FString SkillName;
-        if (JsonObject->TryGetStringField("SkillName", SkillName))
-        {
-            // Then, extract the Phases array from the JsonObject
-            const TArray<TSharedPtr<FJsonValue>>* PhasesArray;
-            if (JsonObject->TryGetArrayField("Phases", PhasesArray))
-            {
-                // iterate PhasesArray
-                for (auto PhaseValue : *PhasesArray)
-                {
-                    TSharedPtr<FJsonObject> PhaseObject = PhaseValue->AsObject();
-                    if (PhaseObject.IsValid())
-                    {
-                        USkillPhase* SkillPhase = NewObject<USkillPhase>(this, USkillPhase::StaticClass());
+		// Assume the JsonString is already deserialized to a FJsonObject called JsonObject
+		// First, extract the SkillName field from the JsonObject
+		FString SkillName;
+		if (JsonObject->TryGetStringField("SkillName", SkillName))
+		{
+			// Then, extract the Phases array from the JsonObject
+			const TArray<TSharedPtr<FJsonValue>>* PhasesArray;
+			if (JsonObject->TryGetArrayField("Phases", PhasesArray))
+			{
+				// iterate PhasesArray
+				for (auto PhaseValue : *PhasesArray)
+				{
+					TSharedPtr<FJsonObject> PhaseObject = PhaseValue->AsObject();
+					if (PhaseObject.IsValid())
+					{
+						USkillPhase* SkillPhase = NewObject<USkillPhase>(this, USkillPhase::StaticClass());
 
-                        // Extract PhaseName and PhaseDuration
-                        if (PhaseObject->TryGetStringField("PhaseName", SkillPhase->PhaseName) && PhaseObject->TryGetNumberField("PhaseDuration", SkillPhase->PhaseDuration))
-                        {
-                            // Extract Nodes array
-                            const TArray<TSharedPtr<FJsonValue>>* NodesArray;
-                            if (PhaseObject->TryGetArrayField("Nodes", NodesArray))
-                            {
-                                // Iterate over each element in the Nodes array and extract the NodeName, Class, and Args fields
-                                for (auto NodeValue : *NodesArray)
-                                {
-                                    TSharedPtr<FJsonObject> NodeObject = NodeValue->AsObject();
-                                    if (NodeObject.IsValid())
-                                    {
-                                        USkillNode* SkillNode = NewObject<USkillNode>(this, USkillNode::StaticClass());
-                                        SkillNode->SetOwner(GetOwner());
+						// Extract PhaseName and PhaseDuration
+						if (PhaseObject->TryGetStringField("PhaseName", SkillPhase->PhaseName) && PhaseObject->TryGetNumberField("PhaseDuration", SkillPhase->PhaseDuration))
+						{
+							// Extract Nodes array
+							const TArray<TSharedPtr<FJsonValue>>* NodesArray;
+							if (PhaseObject->TryGetArrayField("Nodes", NodesArray))
+							{
+								// Iterate over each element in the Nodes array and extract the NodeName, Class, and Args fields
+								for (auto NodeValue : *NodesArray)
+								{
+									TSharedPtr<FJsonObject> NodeObject = NodeValue->AsObject();
+									if (NodeObject.IsValid())
+									{
+										USkillNode* SkillNode = NewObject<USkillNode>(this, USkillNode::StaticClass());
+										SkillNode->SetOwner(GetOwner());
 
-                                        // Extract NodeName and Class
-                                        FString ClassName;
-                                        if (NodeObject->TryGetStringField("NodeName", SkillNode->NodeName) && NodeObject->TryGetStringField("Class", ClassName))
-                                        {
-                                            // Extract Args object
-                                            TSharedPtr<FJsonObject> ArgsObject = NodeObject->GetObjectField("Args");
-                                            if (ArgsObject.IsValid())
-                                            {
-                                                // todo
-                                            }
+										// Extract NodeName and Class
+										FString ClassName;
+										if (NodeObject->TryGetStringField("NodeName", SkillNode->NodeName) && NodeObject->TryGetStringField("Class", ClassName))
+										{
+											// Extract Args object
+											TSharedPtr<FJsonObject> ArgsObject = NodeObject->GetObjectField("Args");
+											if (ArgsObject.IsValid())
+											{
+												// todo
+											}
 
-                                            SkillPhase->Nodes.Add(SkillNode);
-                                        }
-                                    }
-                                }
-                            }
+											SkillPhase->Nodes.Add(SkillNode);
+										}
+									}
+								}
+							}
 
-                            SkillPhases.Add(SkillPhase);
-                        }
-                    }
-                }
-            }
-        }
+							SkillPhases.Add(SkillPhase);
+						}
+					}
+				}
+			}
+		}
 
 	}
 	else
@@ -126,7 +126,7 @@ void USkillCtrlComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 // ClearSkillPhases
 void USkillCtrlComp::ClearSkillPhases()
 {
-    // Then clear the TArray itself.
+	// Then clear the TArray itself.
 	SkillPhases.Empty();
 }
 
